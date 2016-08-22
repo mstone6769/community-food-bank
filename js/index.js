7be8b4c959782126b61e7d01dbcafc4f65a60b60
Morgan Stone <mstone@designbymorgan.com>
2016-08-22 00:00:08 -0500

    contact page

--- js/index.js
+++ js/index.js
@@ -14,3 +14,19 @@ function toggleMenu() {
   var elem = document.getElementById('main-nav');
   elem.classList.toggle('menu-on');
 }
+
+function toggleTab(e, tabsId, tabId) {
+	e.stopImmediatePropagation();
+	var tabsElem = document.getElementById(tabsId);
+	var activeElements = tabsElem.querySelectorAll('.active');
+
+
+	for( index=0; index < activeElements.length; index++ ) {
+		activeElements[index].classList.remove('active');
+ 	}
+
+	var els = tabsElem.querySelector("a[href='#"+tabId+"']");
+	addParentClass(els, 'active');
+
+	document.getElementById(tabId).classList.add('active');
+}
